1f2b62981f54e3974ce10f097b43c222f21f1c38
Chris Neal <me@chrisrneal.com>
2015-12-27 21:19:14 +0000

    Adds logic to be able to search Stocks

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+
+    def my_portolio
+    
+    end
+
+end
---
+++ app/models/stock.rb
@@ -0,0 +1,22 @@
+class Stock < ActiveRecord::Base
+    
+    def self.find_by_ticker(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end 
+    
+    def self.new_from_lookup(ticker_symbol)
+        lookup_stock = StockQuote::Stock.quote(ticker_symbol)
+        return nil unless lookup_stock.name
+        
+        new_stock = Stock.new(ticker:lookup_stock.symbol, name:lookup_stock.name)
+        new_stock.last_price = new_stock.price
+        new_stock
+    end
+    
+    def price
+        ret_val = StockQuote::Stock.quote(ticker).last_trade_price_only
+        return "#{ret_val} (Latest)" if ret_val
+        'Unavailable'
+    end
+
+end
